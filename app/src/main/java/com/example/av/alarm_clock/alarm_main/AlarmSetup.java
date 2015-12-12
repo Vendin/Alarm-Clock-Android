6f75b5005db56acb5dfa00e8443f764fb6f2be84
Mihail Krutyakov <mihanik01@mail.ru>
2015-12-12 14:59:33 +0300

    Added alarm save/update logic in AlarmSetup.

--- app/src/main/java/com/example/av/alarm_clock/alarm_main/AlarmSetup.java
+++ app/src/main/java/com/example/av/alarm_clock/alarm_main/AlarmSetup.java
@@ -2,12 +2,16 @@ package com.example.av.alarm_clock.alarm_main;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TimePicker;
 
+import com.example.av.alarm_clock.Alarm;
 import com.example.av.alarm_clock.R;
+import com.example.av.alarm_clock.storage.AlarmTableHelper;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -38,5 +42,39 @@ public class AlarmSetup extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_alarm_edit, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.saveAction:
+                saveAlarm();
+            case R.id.cancelAction:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    protected void saveAlarm() {
+        int hour = timePicker.getCurrentHour();
+        int minute = timePicker.getCurrentMinute();
+
+        AlarmTableHelper alarmTableHelper = new AlarmTableHelper(this);
+        Alarm alarmToSave = null;
+        if (alarmID != null)
+            alarmToSave = alarmTableHelper.getAlarm(alarmID);
+        if (alarmToSave == null) {
+            alarmToSave = new Alarm();
+            alarmToSave.setHour((short) hour);
+            alarmToSave.setMinute((short) minute);
+            alarmToSave.setEnabled(true);
+            alarmTableHelper.saveAlarm(alarmToSave);
+        } else {
+            alarmToSave.setHour((short) hour);
+            alarmToSave.setMinute((short) minute);
+            alarmTableHelper.updateAlarm(alarmToSave);
+        }
     }
 }
