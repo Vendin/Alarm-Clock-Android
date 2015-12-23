5d65022054c35aadab1a5bc5e5cb1e07ed0b4c3c
Vendin <v.a.5@list.ru>
2015-12-23 18:32:53 +0300

    123

--- app/src/main/java/com/example/av/alarm_clock/alarm_main/AlarmSettings.java
+++ app/src/main/java/com/example/av/alarm_clock/alarm_main/AlarmSettings.java
@@ -2,6 +2,7 @@ package com.example.av.alarm_clock.alarm_main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -26,6 +27,10 @@ public class AlarmSettings extends AppCompatActivity implements SeekBar.OnSeekBa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_settings);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         save = (Button) findViewById(R.id.button_save);
         percentageView = (TextView)findViewById(R.id.label_percentage);
 
@@ -47,28 +52,6 @@ public class AlarmSettings extends AppCompatActivity implements SeekBar.OnSeekBa
         super.onStart();
         seekBar = (SeekBar)findViewById(R.id.entry_percentage);
         seekBar.setOnSeekBarChangeListener(this);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_alarm_settings, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
