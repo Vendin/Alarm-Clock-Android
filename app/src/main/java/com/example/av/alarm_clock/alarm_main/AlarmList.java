c8c6a23678191bdde791c48c2d77cb8d8c224b4f
Mihail Krutyakov <mihanik01@mail.ru>
2015-12-23 12:49:19 +0300

    Fix menu

--- app/src/main/java/com/example/av/alarm_clock/alarm_main/AlarmList.java
+++ app/src/main/java/com/example/av/alarm_clock/alarm_main/AlarmList.java
@@ -5,8 +5,11 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -57,4 +60,21 @@ public class AlarmList extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_alarm_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(this, AlarmSettings.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
